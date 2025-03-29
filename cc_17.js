a5e55a80a7d6473aa72180a9a32bdf8665f50d4e
sprevaux2026 <stevenprevaux@gmail.com>
2025-03-29 07:30:59 -0400

    Task 2 – Created SalesRep class with client management

--- cc_17.js
+++ cc_17.js
@@ -23,3 +23,32 @@ class Customer {
   customer1.addPurchase(150);
   console.log(`Customer: ${customer1.name} | Total Spent: $${customer1.getTotalSpent()}`);
   
+  // Task 2: Create a SalesRep Class
+class SalesRep {
+    constructor(name) {
+      this.name = name;
+      this.clients = []; // Array to store Customer objects
+    }
+  
+    //Add a customer as a client
+    addClient(customer) {
+      this.clients.push(customer);
+    }
+  
+    //Get a specific client's total spent by name
+    getClientTotal(clientName) {
+      const client = this.clients.find(cust => cust.name === clientName);
+      if (client) {
+        return client.getTotalSpent();
+      } else {
+        console.error(`Client ${clientName} not found.`);
+        return 0;
+      }
+    }
+  }
+  
+  //Create a SalesRep, add a customer and log client's total spent
+  const rep1 = new SalesRep('Tony Stark');
+  rep1.addClient(customer1);
+  console.log(`SalesRep: ${rep1.name} | ${customer1.name}'s Total: $${rep1.getClientTotal('Bruce Wayne')}`);
+  
